647806477c02f84ae3f370fa3b4be79eabd0c1e8
Bek-ah <rbkhktvns@gmail.com>
2026-02-12 20:02:59 -0700

    Stable version routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,60 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+import { Teacher } from './teacher/teacher';
 
 export default function App() {
   return (
-        <header>
-            <h1>AtoG</h1>
-        </header>
+      <BrowserRouter>
+      <header>
+          <h1>AtoG</h1>
+                <NavLink>
+                <menu className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="play">
+                    Play
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="scores">
+                    Scores
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="about">
+                    About
+                  </NavLink>
+                </li>
+              </menu>
+            </NavLink>
+      </header>
 
-        <main>App components go here</main>
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/play' element={<Play />} />
+        <Route path='/scores' element={<Scores />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
 
         <footer>
             <span class="text-reset">Rebekah Evans</span>
-            <a href="https://github.com/Bek-ah/chess.git">Github Link</a>
+            <NavLink to="https://github.com/Bek-ah/chess.git">Github Link</NavLink>
         </footer>
-      );
+        </BrowserRouter>
+
+  );
+}
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
