4b692f0ae032e24c599e61a3af05d15a4803c24d
Bek-ah <rbkhktvns@gmail.com>
2026-02-12 19:43:31 -0700

    Stable version app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+        <header>
+            <h1>AtoG</h1>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer>
+            <span class="text-reset">Rebekah Evans</span>
+            <a href="https://github.com/Bek-ah/chess.git">Github Link</a>
+        </footer>
+      );
 }
